781ba70cb9d48f407f4fe229736935a97b032206
geekbrains <info@geekbrains.ru>
2022-12-03 12:06:18 +0500

    tas41

---
+++ task41/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите числа через пробел: ");
+int[] arrey = Array.ConvertAll(Console.ReadLine().Split(),int.Parse); // вывод массива сразу всеми вводимыми числами
+
+int NumbersInArrey(int[] arrey)
+{
+    int num = 0;
+    for (int i = 0; i < arrey.Length; i++)
+    {
+        if (arrey[i] > 0) num++;
+    }
+    return num;
+}
+int num = NumbersInArrey(arrey);
+Console.WriteLine($"Пользователем введено {num} числа/чисел больше нуля");
